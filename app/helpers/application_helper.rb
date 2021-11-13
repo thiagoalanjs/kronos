819cf9ee14ae56f3eb71100f874eb6ddbbcca3cf
Thiago Alan <thiagoalan9080@gmail.com>
2021-11-13 02:18:29 -0300

    Ajuste alerts devise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,13 @@ module ApplicationHelper
   def status_labels
     return {1 => 'Pendente', 2 => 'Desenvolvimento', 3 => 'Code Review', 4 => 'QA', 5 => 'Concluída'}
   end
+
+  def alert_for(flash_type){
+       success: 'ui success message',
+       alert: 'ui negative message',
+       notice: 'ui negative message',  
+       warning: 'ui negative message'
+      }[flash_type.to_sym] || flash_type.to_s
+  end
+ 
 end
