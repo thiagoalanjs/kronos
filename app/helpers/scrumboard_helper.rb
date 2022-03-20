b0a339ccca1b104af739c6379ced3eb8ac7250b7
Thiago Alan <thiagoalan9080@gmail.com>
2022-03-19 23:38:21 -0300

    Fix sprint bug

--- app/helpers/scrumboard_helper.rb
+++ app/helpers/scrumboard_helper.rb
@@ -1,10 +1,20 @@
 module ScrumboardHelper
+
     def sprint_actived
         Sprint.find_by_sql("SELECT * from sprints 
                                 where sprint_status == 'ATIVA'
-                                AND project_id = #{ current_project_id } ")
+                                AND project_id = #{ current_project_id } order by id desc limit 1 ")
     end
 
+    def last_sprint
+        sprint = ActiveRecord::Base.connection
+        res_query = sprint.execute("SELECT sprint_status from sprints
+                                   WHERE project_id = #{ current_project_id } 
+                                   order by id desc limit 1")  
+        delete_chars = res_query.to_s                            
+        delete_chars.delete('[{"sprint_status"=>""}]')                           
+    end
+  
     def tasks_pending_count
         Task.find_by_sql("SELECT * FROM tasks
                   INNER JOIN user_stories ON user_stories.sprint_id = sprint 
